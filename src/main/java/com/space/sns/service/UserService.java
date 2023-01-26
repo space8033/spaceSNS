c81351b7f2d36729c3e0fe4741d489b0afb19f3f
space8033 <space8237@naver.com>
2023-01-26 16:08:20 +0900

    init : alarmTest complete

--- src/main/java/com/space/sns/service/UserService.java
+++ src/main/java/com/space/sns/service/UserService.java
@@ -8,6 +8,8 @@ import com.space.sns.repository.UserEntityRepository;
 import com.space.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -51,5 +53,10 @@ public class UserService {
         String token = JwtTokenUtils.generateToken(userName, secretKey, expiredTimeMs);
 
         return token;
+    }
+
+    public Page<Void> alarmList(String userName, Pageable pageable) {
+
+        return Page.empty();
     }
 }
