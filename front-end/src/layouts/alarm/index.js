84669bf4efdc75b3b97e5d38a511846e0312a2ab
space8033 <space8237@naver.com>
2023-02-03 14:24:15 +0900

    feat : event source url 원상복귀

--- front-end/src/layouts/alarm/index.js
+++ front-end/src/layouts/alarm/index.js
@@ -98,7 +98,7 @@ function Alarm() {
   useEffect(() => {
     handleGetAlarm();
 
-    eventSource = new EventSource("https://localhost:8080/api/v1/users/alarm/subscribe?token=" + localStorage.getItem('token'));
+    eventSource = new EventSource("http://localhost:8080/api/v1/users/alarm/subscribe?token=" + localStorage.getItem('token'));
 
     setAlarmEvent(eventSource);
 
