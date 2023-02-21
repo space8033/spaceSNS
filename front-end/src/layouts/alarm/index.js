28e076494635455c0ff34182a3985ab020f784ff
space8033 <space8237@naver.com>
2023-02-21 15:03:24 +0900

    chore : eventSource 주소 수정

--- front-end/src/layouts/alarm/index.js
+++ front-end/src/layouts/alarm/index.js
@@ -98,7 +98,7 @@ function Alarm() {
   useEffect(() => {
     handleGetAlarm();
 
-    eventSource = new EventSource("http://localhost:8080/api/v1/users/alarm/subscribe?token=" + localStorage.getItem('token'));
+    eventSource = new EventSource("http://snsspace.herokuapp.com/api/v1/users/alarm/subscribe?token=" + localStorage.getItem('token'));
 
     setAlarmEvent(eventSource);
 
